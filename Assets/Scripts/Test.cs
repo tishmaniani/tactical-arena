865a346659bec23e3c31571f873c74e579aa0b75
Serho <supertishman@yandex.ru>
2026-02-12 20:06:01 +0300

    test commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Для теста");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

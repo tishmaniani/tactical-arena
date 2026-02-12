b385646488fbb9db7f2d60d5a947848652d136f5
Serho <supertishman@yandex.ru>
2026-02-12 20:40:25 +0300

    test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using UnityEngine;
 
+
 public class Test : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created

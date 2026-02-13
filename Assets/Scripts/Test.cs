a16a62a424192a6e5c5b77446d284913eb40dbef
Serho <supertishman@yandex.ru>
2026-02-13 20:15:48 +0300

    Merge branch 'main' of https://github.com/tishmaniani/tactical-arena

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Для теста");
+        UnityEngine.Debug.Log("Для теста");
     }
 
     // Update is called once per frame

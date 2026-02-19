3cb82de49a15a7e19c53be17c05d99008feee974
Serho <supertishman@yandex.ru>
2026-02-19 13:16:23 +0300

    rotation

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -7,13 +7,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private UnitData _data;
 
     private Vector2 _moveInput;
+    private Vector2 _mousePosition;
+    private Camera _mainCamera;
 
-    private void OnMove(InputValue value) 
+    void Awake()
+    {
+        _mainCamera = Camera.main; //Кэшируем камеру для производительности   
+    }
+
+    public void OnMove(InputValue value)
     {
         _moveInput = value.Get<Vector2>();
     }
 
-// InputValue value - Это специальный «контейнер» (объект-обертка), который Unity присылает вместе с событием. Внутри него лежат данные о нажатии.
+    public void OnLook(InputValue value)
+    {
+        _mousePosition = value.Get<Vector2>();
+    }
+
+
+    // InputValue value - Это специальный «контейнер» (объект-обертка), который Unity присылает вместе с событием. Внутри него лежат данные о нажатии.
 
     void Start()
     {
@@ -22,12 +35,40 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Move();
+        HandleMovement();
+        HandleRotation();
     }
-    private void Move()
+
+    private void HandleMovement()
     {
-        Vector3 movement = new Vector3(_moveInput.x, 0, _moveInput.y) * _data.Speed * Time.deltaTime;
-        transform.Translate(movement);
+        Vector3 direction = new Vector3(_moveInput.x, 0, _moveInput.y);
+        transform.Translate(direction * _data.Speed * Time.deltaTime, Space.World);
+    }
+
+    private void HandleRotation()
+    {
+        Ray ray = _mainCamera.ScreenPointToRay(_mousePosition);
+        
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
+
+
+        Debug.Log(ray.origin);
+        Debug.Log(ray.direction);
+
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+
+        if (groundPlane.Raycast(ray, out float rayDistance))
+        {
+            Vector3 hitPoint = ray.GetPoint(rayDistance);
+
+            Vector3 lookDirection = hitPoint - transform.position;
+            lookDirection.y = 0;
+
+            if (lookDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(lookDirection);
+            }
+        }
     }
 
 }
